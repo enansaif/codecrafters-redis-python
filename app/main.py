61f027a6f075447fafa501956c6a3731e32ba158
rect1fy <enansaifme33@gmail.com>
2024-11-06 16:20:05 +0600

    empty message fix

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, _ = server_socket.accept()
     message = "+PONG\r\n".encode()
-    client_socket.sendall()
+    client_socket.sendall(message)
 
 
 if __name__ == "__main__":
