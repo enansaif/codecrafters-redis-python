c2c0801a47284a8169ad9508d27dfc62cfb6eb2b
rect1fy <enansaifme33@gmail.com>
2024-11-07 16:45:02 +0600

    multi request test

--- app/main.py
+++ app/main.py
@@ -5,11 +5,13 @@ def main():
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.listen(5)
+
     while True:
         client_socket, _ = server_socket.accept()
         message = "+PONG\r\n".encode()
         client_socket.sendall(message)
-
+        client_socket.close()
 
 if __name__ == "__main__":
     main()
