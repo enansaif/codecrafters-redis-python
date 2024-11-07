2c2375c0a01a17cffd599e8d7709ce675e0f84af
rect1fy <enansaifme33@gmail.com>
2024-11-07 16:33:48 +0600

    support for multiple response

--- app/main.py
+++ app/main.py
@@ -5,9 +5,10 @@ def main():
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept()
-    message = "+PONG\r\n".encode()
-    client_socket.sendall(message)
+    while True:
+        client_socket, _ = server_socket.accept()
+        message = "+PONG\r\n".encode()
+        client_socket.sendall(message)
 
 
 if __name__ == "__main__":
