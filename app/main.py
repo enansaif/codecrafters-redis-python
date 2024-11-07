65345385c8262a05f1b694e437e9d73616bdde08
rect1fy <enansaifme33@gmail.com>
2024-11-07 16:55:36 +0600

    test

--- app/main.py
+++ app/main.py
@@ -5,12 +5,15 @@ def main():
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.listen(5)
+    client_socket, _ = server_socket.accept()
 
     while True:
-        client_socket, _ = server_socket.accept()
-        message = "+PONG\r\n".encode()
-        client_socket.sendall(message)
+        request = client_socket.recv(1024)
+        data = request.decode()
+        if "ping" in data.lower():
+            message = "+PONG\r\n".encode()
+            client_socket.sendall(message)
+            break
 
 if __name__ == "__main__":
     main()
