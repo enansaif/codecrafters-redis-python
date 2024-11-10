7146ba8fe2078c2bb6fdd0531d0f89abc78d0215
rect1fy <enansaifme33@gmail.com>
2024-11-10 18:01:08 +0600

    concurrent connections

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 import socket
+import threading
 
+def handle_request(connection):
+    response = "+PONG\r\n".encode()
+    with connection:
+        connected = True
+        while connected:
+            data = connection.recv(512)
+            data_str = data.decode().lower()
+            if "ping" in data_str:
+                connection.sendall(response)
 
 def main():
     print("Logs from your program will appear here!")
-    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept()
-
+    
     while True:
-        request = client_socket.recv(1024)
-        data = request.decode()
-        if "ping" in data.lower():
-            message = "+PONG\r\n".encode()
-            client_socket.sendall(message)
+        client_socket, _ = server_socket.accept()
+        thread = threading.Thread(target=handle_request, args=[client_socket])
+        thread.start()
 
 if __name__ == "__main__":
     main()
